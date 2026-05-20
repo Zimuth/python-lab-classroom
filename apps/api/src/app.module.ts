e3140ba6238f89a217e9a61c62488c84aaa90dbe
Juan Butron <yosoyunclap@gmail.com>
2026-05-20 01:23:25 -0400

    feat: Pruebas con la creacion de base de datos con docker

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5555,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'python_lab',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
---
+++ apps/api/src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return this.usersService.create(body);
+  }
+}
