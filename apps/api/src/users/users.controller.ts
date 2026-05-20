6fc1317fe61793fd7dfbb6c86fca793ee3cc9b52
Juan Butron <yosoyunclap@gmail.com>
2026-05-20 10:09:55 -0400

    feat: Funcion aplicada seguridad a la contraseña

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -12,7 +13,7 @@ export class UsersController {
   }
 
   @Post()
-  create(@Body() body: any) {
+  create(@Body() body: CreateUserDto) {
     return this.usersService.create(body);
   }
 }
--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -11,12 +12,12 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
-  findAll() {
-    return this.usersRepository.find();
+  create(createUserDto: CreateUserDto) {
+    const user = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(user);
   }
 
-  create(userData: Partial<User>) {
-    const user = this.usersRepository.create(userData);
-    return this.usersRepository.save(user);
+  findAll() {
+    return this.usersRepository.find();
   }
 }
