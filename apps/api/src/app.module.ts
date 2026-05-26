75c3c1c7837b92861093744021fa3ddae90de4b0
Juan Butron <yosoyunclap@gmail.com>
2026-05-26 18:49:22 -0400

    Fix: Correccion de configuracion de pipeline en ci.yml

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,20 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5555,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'python_lab',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
+
     UsersModule,
   ],
 })
