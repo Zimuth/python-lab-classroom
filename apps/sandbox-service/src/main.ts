d8955e7e8aaf6e59e5177b3b374144580c940cf1
Celier Choque <celier98jaime@gmail.com>
2026-06-08 00:02:51 -0400

    test: fix test

--- apps/sandbox-service/src/main.ts
+++ apps/sandbox-service/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
-   = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(process.env.PORT ?? 3000);
 }
