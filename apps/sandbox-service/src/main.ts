aa6ff25ccca94b929b628a6d8579c0399f38da73
Juan Butron <yosoyunclap@gmail.com>
2026-06-07 23:40:10 -0400

    test 2 en sandobox

--- apps/sandbox-service/src/main.ts
+++ apps/sandbox-service/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(process.env.PORT ?? 3000);
 }
